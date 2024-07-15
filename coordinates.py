c6a9da4d6705969324d0b1d3ebf64981b2ca2109
Joel Sousa <joel30052000@gmail.com>
2024-07-15 07:59:42 +0100

    Update coordinates.py
    Main changes:
    - Added install_and_import to automatically install the python packages used in the script that the user might not have had installed in the machine 
    - Fixed syntax in how the main function is called
    - Changed the written file from txt to ics, so that it could be uploaded to a chalendar app with ease.
    
    Still to fix:
    - Name of the event in the chalendar: needs to be shortened to include only the client number and actual name. Other information included must be transferred to the description
    
    
    Possible features:
    - Create a function that checks if coordinates exist in the file and use either the address or the coordinates in the location field of the ics file

--- coordinates.py
+++ coordinates.py
@@ -1,66 +1,80 @@
-#This script has the following objectives:
+# This script has the following objectives:
+# - Access the file containing information about the work schedule for the whole year
+# - Identify the current day when the script is run
+# - Collect relevant information for the next seven days, such as:
+#   - Clients to visit
+#   - Scheduled visit times
+#   - Type of measurements (CQI, CQE, CQE.EXTRA, TA)
+#   - Equipment to be evaluated (INTRA, ORTO, ORTOCEF, ORTOCBCT, ORTOCBCTCEF, etc.)
+#   - Client address
+# - Generate a .txt file named "Week_Schedule.txt" with the collected data
 
-#To access the file containing information about the work schedule for the whole year
-#To identify what day it is at the time of running the script
-#To collect the relevant information for the next seven days, such as:
-#   Clients to visit;
-#   Time scheduled to visit;
-#   What type of measurements are to be taken (CQI, CQE, CQE.EXTRA or TA)
-#   What equipments are to be evaluated (INTRA, ORTO, ORTOCEF, ORTOCBCT, ORTOCBCTCEF,...)
-#   The address of the client
-#After collecting such data, a .txt file, by the name of "Week_Schedule.txt" is to be generated
-#The purpose of this file is to facilitate the worker navigation, by creating a file easy to copy to apps like google maps or calendar
-
-#    LIBS
-from datetime import date
-import pandas as pd #requires pyarrow
-import re
+import subprocess
+import sys
 import os
-from os import listdir
-import xlrd
+from datetime import date, datetime, timedelta
+import re
 import io
+from os import listdir
+# Install required packages if not already installed
+required_packages = [
+    'pandas',
+    'xlrd',
+    'openpyxl',
+    'msoffcrypto-tool',
+    'ics'
+]
+
+def install_and_import(package):
+    try:
+        __import__(package)
+    except ImportError:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+# Ensure all required packages are installed
+for package in required_packages:
+    install_and_import(package)
+
+import pandas as pd
+import xlrd
 import msoffcrypto
 import openpyxl
-
-
+from ics import Calendar, Event
 
 def get_days():
     today = date.today()
 
-    #converting datetime day into int
-
+    # Converting datetime day into int
     day_start = int(today.strftime("%d"))
     month_start = int(today.strftime("%m"))
     year = int(today.strftime("%Y"))
-    day_end = int(today.strftime("%d"))+7
+    day_end = day_start + 7
     month_end = month_start
-    bigMonths = [1,3,5,7,8,10,12]
-    smallMonths = [4,6,9,11]
+    bigMonths = [1, 3, 5, 7, 8, 10, 12]
+    smallMonths = [4, 6, 9, 11]
 
-# Adjusting values when the week is present in 2 seperate months
-
-    if month_start==2 and year%4==0:
-        if day_end>29:
-            month_end = month_start+1
-            day_end = day_end-29
-    elif month_start==2 and year%4!=0:
+    # Adjusting values when the week spans two months
+    if month_start == 2 and year % 4 == 0:
+        if day_end > 29:
+            month_end = month_start + 1
+            day_end -= 29
+    elif month_start == 2 and year % 4 != 0:
         if day_end > 28:
-            month_end = month_start+1
-            day_end = day_end-28
+            month_end = month_start + 1
+            day_end -= 28
     elif month_start in smallMonths:
-        if day_end>30:
-            month_end = month_start+1
-            day_end = day_end-30
+        if day_end > 30:
+            month_end = month_start + 1
+            day_end -= 30
     elif month_start in bigMonths:
-        if day_end>31:
-            month_end = month_start+1
-            day_end = day_end-31
+        if day_end > 31:
+            month_end = month_start + 1
+            day_end -= 31
 
-
-    #Creating a dictionary for easy access to information on dates 
+    # Creating a dictionary for easy access to information on dates
     dateDict = {
         "year": year,
-        "start_month" : month_start,
+        "start_month": month_start,
         "end_month": month_end,
         "start_day": day_start,
         "end_day": day_end
@@ -70,31 +84,24 @@ def get_days():
 
 # List all files' names in a directory
 def listFiles(cwd):
-    allFiles = []
-    allFiles = listdir(cwd)
-    return allFiles
+    return listdir(cwd)
 
-
-
-# will search for the excel file that has the current Year schedule
+# Search for the Excel file that has the current year's schedule
 def matched_Excel(currentYear):
-    fullCWD = os.getcwd()#Current Working Directory Full path
+    fullCWD = os.getcwd()  # Current Working Directory Full path
     allFiles = listFiles(fullCWD)
-    currentYear=str(currentYear)
-    regex = r"(.*)"+currentYear+" - PRO\.APL\.\d{,3} - AGENDA"+"(.*)"
+    currentYear = str(currentYear)
+    regex = r"(.*)" + currentYear + r" - PRO\.APL\.\d{1,3} - AGENDA.*"
+    
     for file_name in allFiles:
-        matches= re.search(regex,file_name)
+        matches = re.search(regex, file_name)
         if matches is not None:
             name = matches.group(1).strip()
             file_matched = matches.group(0)
-            break
-        else:
-            continue    
-    return file_matched, name
+            return file_matched, name
+    raise FileNotFoundError("No matching Excel file found.")
 
-
-# will open the corresponding excel and send the data via dataframe format
-
+# Open the corresponding Excel file and return the data as a dataframe
 def open_Excel(matchedExcel):
     decrypted_workbook = io.BytesIO()
     with open(matchedExcel, 'rb') as file:
@@ -105,138 +112,108 @@ def open_Excel(matchedExcel):
     wb = openpyxl.load_workbook(filename=decrypted_workbook, data_only=True)
     return wb
 
-
-# Made to reuse code
-def sheetinfo(wb,sheetName):
+# Reuse code for extracting sheet info
+def sheetinfo(wb, sheetName):
     ws = wb[sheetName]
-    df = pd.DataFrame(ws.values) 
-    indices = [1,2,3,4,5,6,10]
-    
-    info = df.iloc[:,indices]
+    df = pd.DataFrame(ws.values)
+    indices = [1, 2, 3, 4, 5, 6, 10]
 
-    info.columns = ['Date', 'Start', 'Finish', 'Client', 'Serv','Equips','Obs']
-    
+    info = df.iloc[:, indices]
+    info.columns = ['Date', 'Start', 'Finish', 'Client', 'Serv', 'Equips', 'Obs']
     
     return info
 
-
-
 def sheetDF(wb, dateDict):
-    
-    if len(str(dateDict['start_month']))<2:
-        dateDict['start_month'] = '0'+str(dateDict['start_month'])
-    else:
-        dateDict['start_month'] = str(dateDict['start_month'])
-    
-    if len(str(dateDict['end_month']))<2:
-        dateDict['end_month'] = '0'+str(dateDict['end_month'])
-    else:
-        dateDict['end_month'] = str(dateDict['end_month'])
+    dateDict['start_month'] = f"{dateDict['start_month']:02}"
+    dateDict['end_month'] = f"{dateDict['end_month']:02}"
 
     sheetNameStart = str(dateDict['year'])[-2:] + dateDict['start_month']
-
     sheetNameEnd = str(dateDict['year'])[-2:] + dateDict['end_month']
     
-    infoStart = sheetinfo(wb,sheetNameStart)
-    
-    infoEnd = sheetinfo(wb,sheetNameEnd)
+    infoStart = sheetinfo(wb, sheetNameStart)
+    infoEnd = sheetinfo(wb, sheetNameEnd)
 
     return infoStart, infoEnd
 
-#Decided to create a function purely to get the date string so that code is reused and lines aren't repeated
-def getDateFormat(startYear,startDay, startMonth):
-    #Adjusting the string to get the correct expression, according to how the dataframe expresses date: "YYYY-MM-DD"
-    if len(str(startMonth))<2:
-        startMonth = '0'+str(startMonth)
-    else:
-        startMonth = str(startMonth)
-    #Same adjustment for day
-    if len(str(startDay))<2:
-        startDay = '0'+str(startDay)
-    else:
-        startDay = str(startDay)
+def getDateFormat(year, day, month):
+    # Adjust string to get the correct expression, according to how the dataframe expresses date: "YYYY-MM-DD"
+    return f"{year}-{month:02}-{day:02}"
 
-    #Get the full date in the same format as the excel cells
-    
-    startDate = str(startYear)+'-'+str(startMonth)+'-'+str(startDay)  
+def searchDate(infoStart, infoEnd, dateDict):
+    startDate = getDateFormat(dateDict['year'], dateDict['start_day'], dateDict['start_month'])
+    endDate = getDateFormat(dateDict['year'], dateDict['end_day'], dateDict['end_month'])
 
-    return startDate
+    indexStart = next((i for i, row in enumerate(infoStart.iloc[:, 0]) if startDate in str(row)), None)
+    indexEnd = next((i for i, row in enumerate(infoEnd.iloc[:, 0]) if endDate in str(row)), None)
 
-#This function should return the indexes for he 1st cell that matches a given date
-def searchDate(infoStart, infoEnd,dateDict):
-  
-    startDate = getDateFormat(dateDict['year'],dateDict['start_day'],dateDict['start_month'])
+    if indexStart is None or indexEnd is None:
+        raise ValueError("Start or end date not found in the data.")
 
-    indexStart = 0
+    return indexStart, indexEnd
 
-    for row in infoStart.iloc[:,0]:
-        if startDate in str(row):
-            break
-        else:
-            indexStart+=1
-    
-    indexEnd = 0
-    
-    endDate = getDateFormat(dateDict['year'],dateDict['end_day'],dateDict['end_month'])
-    for row in infoEnd.iloc[:,0]:
-        if endDate in str(row):
-            break
-        else:
-            indexEnd+=1
-    return indexStart , indexEnd
-
-
-#This function will filter through the dataframe information to only keep the relevant rows
 def filterInfo(indexStart, indexEnd, infoStart, infoEnd):
-
-    #Merging both dataframes to include all the information in one dataframe
+    # Merging both dataframes to include all the information in one dataframe
     if infoStart.equals(infoEnd):
-        mergedInfo = infoStart.iloc[indexStart:indexEnd,:]
+        mergedInfo = infoStart.iloc[indexStart:indexEnd, :]
     else:
-        startInterval = infoStart.iloc[indexStart:,:]
-        endInterval = infoEnd.iloc[2:indexEnd,:]
-        mergedInfo = pd.concat([startInterval,endInterval])
+        startInterval = infoStart.iloc[indexStart:, :]
+        endInterval = infoEnd.iloc[2:indexEnd, :]
+        mergedInfo = pd.concat([startInterval, endInterval])
+
+   #Changing the None value in the date to the date in the previous row
+    for index in range(len(mergedInfo['Date'])):
+       if mergedInfo.iloc[index,0]==None:
+           mergedInfo.iloc[index,0]=mergedInfo.iloc[index-1,0]
+
     
-    #resetting the rows index
-    mergedInfo = mergedInfo.reset_index()
-    # Defining expressions that don't need to be included:
-    expressions = ["https",
-                   "None",
-                   "deslocação",
-                   "CLIENTE",
-                   "EQUIPs",
-                   "OBSERVAÇÕES TÉCNICO"]
-    #Filtering the rows that include pointless information
-    mergedInfo = mergedInfo.drop('index',axis=1)
-    index_list = []
-    for row_index in range(len(mergedInfo)):
-        for expression in expressions:
-            if expression in str(mergedInfo.iloc[row_index,3]):
-                index_list.append(row_index)
-        
-
+    # Resetting the row index
+    mergedInfo = mergedInfo.reset_index(drop=True)
+    
+    # Defining expressions that don't need to be included
+    expressions = ["https", "None", "deslocação", "CLIENTE", "EQUIPs", "OBSERVAÇÕES TÉCNICO"]
+    
+    # Filtering the rows that include pointless information
+    index_list = [row_index for row_index, row in mergedInfo.iterrows()
+                  if any(expression in str(row['Client']) for expression in expressions)]
+    
     finalInfo = mergedInfo.drop(index_list)
-
+    
     return finalInfo
 
-# This function will write the python file with the required information.
-# This will detect when the lines have unwarrented information and won'te write them
-
-def writeTXT(df):
-
-    with open("./coordinates.txt",'w', encoding="utf-8") as f: #encoding is specified to include special characters in the txt file
-        df_string = df.to_string()
-        f.write(df_string)
-
-    return 0 #done to not get error message because function is yet to be correctly scripted
+def writeICS(df):
+    
+    cal = Calendar()
+    for index, row in df.iterrows():
+        event = Event()
+        event.name = f"{row['Client']} - {row['Serv']}"
+        
+        if row['Start'] == None or pd.isna(row['Start']):
+            event.begin = datetime(row['Date'].year, row['Date'].month,row['Date'].day)
+            event.end = datetime(row['Date'].year, row['Date'].month,row['Date'].day)
+            
+        else:
+            print(type(row['Start']))
+            event.begin = datetime(int(row['Date'].year),int(row['Date'].month), int(row['Date'].day), int(row['Start'].hour), int(row['Start'].minute))
+            
+            event.end = datetime(int(row['Date'].year), row['Date'].month, row['Date'].day, row['Finish'].hour, row['Finish'].minute)
+        
+        
+        event.description = f"Equipments: {row['Equips']}\nObservations: {row['Obs']}"
+        cal.events.add(event)
+    with open("Week_Schedule.ics", 'w', encoding="utf-8") as f:  # Encoding is specified to include special characters in the txt file
+        
+        f.writelines(cal)
+    return(cal)
 
 def main():
     dateDict = get_days()
     fileMatched, userName = matched_Excel(dateDict['year'])
     wb = open_Excel(fileMatched)
-    infoStart, infoEnd = sheetDF(wb,dateDict)
-    indexStart, indexEnd = searchDate(infoStart, infoEnd,dateDict)
-    df = filterInfo(indexStart,indexEnd,infoStart,infoEnd)
-    writeTXT(df)
-    
-main()
+    infoStart, infoEnd = sheetDF(wb, dateDict)
+    indexStart, indexEnd = searchDate(infoStart, infoEnd, dateDict)
+    df = filterInfo(indexStart, indexEnd, infoStart, infoEnd)
+    cal = writeICS(df)
+    print(cal)
+
+if __name__ == "__main__":
+    main()
